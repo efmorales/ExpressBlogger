7d57ec959afed00199e68e6e43c3e978c87508ab
efmorales <efmorales@pucp.edu.pe>
2023-01-31 15:54:47 -0500

    added blogs.js with get and delete tests

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 // setup router for each set of routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const blogsRouter = require('./routes/blogs');
 // instantiate the actual express app
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // we bind (associate) the routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogs', blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
