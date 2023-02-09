d963a0e86e030a24ae71309ea0e25917230f4d59
efmorales <efmorales@pucp.edu.pe>
2023-02-09 14:46:01 -0500

    Stretch goals completed. A rework of the project, added the mongo.js file to connect the sample MongoDB database. blogs.js has been heavily modified to get valid requests from the browser and postman

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
-// instantiate libraries
+//instantiate standard libraries
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// setup router for each set of routes
+
+//loads the contents of config.env
+require("dotenv").config({path: './config.env'});
+
+var { mongoConnect } = require('./mongo.js');
+mongoConnect();
+
+//setup router for each set of routes 
+// importing from routes/ folder 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const blogsRouter = require('./routes/blogs');
-// instantiate the actual express app
+
+//instantiate the actual express app
 const app = express();
 
+
 // view engine setup
+// sets application settings. (things we can access across the application)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-// associating the libraries with the app
+
+//associating the libraries with the app
+// adding middleware 
+//(adding libraries that we can use throughout our application)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// for hosting static files: css, html, images, etc
-app.use(express.static(path.join(__dirname, 'public')));
 
-// we bind (associate) the routes
+//for hosting static files: css, html, images etc. 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+//we bind (associate) the routers to routes in our application
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/blogs', blogsRouter);
@@ -41,10 +56,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+}); 
+
+
 
 // app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
+//   console.log(`ExpressBlogger app listening on port ${port}`)
 // })
 
 module.exports = app;
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,55 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+//instantiate mongodb 
+const { db } = require('../mongo');
+
 const { validateBlogData } = require("../validation/blogs");
 
-const sampleBlogs = [
-    {
-        title: "dicta",
-        text: "Iusto et in et. Nulla accusantium fugit. Et qui dolorem inventore soluta et veritatis. Aut ut aut non laudantium eveniet suscipit odit. Sapiente sint nihil nihil sit et molestias. In nisi omnis quas et sed aut minus aperiam ea.\n \rLaudantium quo quisquam quae. Et et quas officia perspiciatis iusto sunt sunt eaque. Quidem sit voluptas deserunt sequi magni.\n \rEst est facere cumque ipsam omnis animi. Voluptatem magnam officiis architecto possimus. Quia similique aut eos qui. Quasi quae sed aliquam.",
-        author: "Darren Abbott",
-        category: ["Lorem", "sit", "amet"],
-        createdAt: "2022-03-22T10:36:37.176Z",
-        lastModified: "2022-03-22T10:36:37.176Z",
-    },
-    {
-        title: "ducimus",
-        text: "Placeat ea et fuga. Qui itaque quibusdam nam. Maxime nobis quam. Et laudantium sunt incidunt reiciendis.\n \rEarum aut sed omnis autem aliquam architecto corporis sint. Nostrum cumque voluptatem aperiam alias similique. Tenetur et esse omnis praesentium ipsum alias. Impedit rerum qui quia quaerat architecto mollitia est autem. Qui blanditiis earum et qui dolorum reprehenderit. Debitis est temporibus.\n \rEt nam sed. Corporis ut rerum. Ut qui dolore est dolorem ex.",
-        author: "Luke Rogahn PhD",
-        category: ["Lorem", "ipsum"],
-        createdAt: "2022-03-22T15:16:56.285Z",
-        lastModified: "2022-03-22T15:16:56.285Z",
-    },
-    {
-        title: "quod",
-        text: "Accusamus nisi eos. Tenetur earum tenetur nemo. Qui voluptas temporibus repellendus maxime. Ipsum optio voluptate enim nihil. Ea et dolorem. Omnis unde perspiciatis.\n \rUt odio eaque. Harum non placeat. Eveniet molestiae in cupiditate dolor doloremque rerum eligendi aut ab.\n \rMolestias eligendi et. Nemo velit natus autem numquam atque provident et nulla. In et dolores ad nihil. Delectus quis doloremque asperiores similique. Asperiores id nam vitae nobis labore autem. Dolor aperiam provident quia consectetur aut ut.",
-        author: "Maryann Schneider",
-        category: ["Lorem", "ipsum", "dolor", "sit", "amet"],
-        createdAt: "2022-03-21T20:09:32.298Z",
-        lastModified: "2022-03-21T20:09:32.298Z",
-    },
-    {
-        title: "ut",
-        text: "Itaque necessitatibus repudiandae. Porro suscipit exercitationem qui atque. Perferendis suscipit debitis sint aut dignissimos nobis ut. Modi ea nihil est vel consequuntur voluptatem. In magnam delectus in eos reiciendis sit est enim eligendi. Sint dicta at.\n \rConsectetur aspernatur alias sed non explicabo blanditiis laborum fugit voluptate. Reiciendis iste aut sit natus qui et in ratione. Placeat qui in voluptatum autem nulla ratione. Commodi sit alias sint sapiente rem. Quia sapiente minus deleniti vitae.\n \rExercitationem numquam omnis maxime dolorum sed deserunt suscipit laudantium. Ad et autem voluptatem esse laudantium et. Id fuga accusamus est sapiente dicta.",
-        author: "Dr. Lorenzo Anderson",
-        category: ["ipsum", "dolor", "sit", "amet"],
-        createdAt: "2022-03-21T23:07:53.447Z",
-        lastModified: "2022-03-21T23:07:53.447Z",
-    },
-    {
-        title: "id",
-        text: "Porro officia aliquid fugiat sed reprehenderit illo amet doloribus sed. Molestiae vero et. Quae voluptates dolores. Voluptatem facere fuga. Veniam perferendis illo ut sunt earum deleniti.\n \rIusto neque dolorem esse error. Saepe et quia ut corrupti. Autem repellendus similique dolorem sunt in ipsa perferendis. Et excepturi ut voluptatem deserunt accusantium dolores aperiam cum ut.\n \rDoloremque expedita sit et voluptatem unde libero. Numquam beatae sed repellat iusto doloribus fugit tenetur. Possimus et ut adipisci harum voluptatem provident consequatur. Corporis quo aut vel itaque blanditiis illum.",
-        author: "Bobbie Dach",
-        category: ["amet"],
-        createdAt: "2022-03-22T15:14:39.819Z",
-        lastModified: "2022-03-22T15:14:39.819Z",
-    },
-];
+/* GET blogs listing. */
+router.get('/', async function(req, res, next) {
+    const blogs = await db()
+    .collection('sample_blogs')
+    .find({})
+    .limit(15)
+    .toArray(function(err, result){
+        if (err) {
+          res.status(400).send("error fetching blogs")
+        } else {
+          res.json(result);
+        }
+      }); 
+  
+      res.json({
+        sucess:true,
+        blogs: blogs
+      });
+  
+      
+  }); 
 
-// POST blog
+// Post ("create-one") a new blog
 
-
-router.post("/create-one", (req, res) => {
+router.post("/create-one", async (req, res) => {
 
     //try block, for validation code
     try {
@@ -78,9 +59,11 @@ router.post("/create-one", (req, res) => {
             throw Error(blogDataCheck.message)
         }
 
-        sampleBlogs.push(blogData);
+        const insertResponse = await db()
+        .collection('sample_blogs')
+        .insertOne(blogData)
 
-        console.log("blogList ", sampleBlogs);
+        console.log("blogList ", blogData);
 
         res.status(200);
         res.json({
@@ -96,46 +79,109 @@ router.post("/create-one", (req, res) => {
     }
 });
 
-/* GET blogs listing. */
+// get just one post from all the db posts
 
-router.get('/', function (req, res, next) {
-    res.send('blogs page');
-});
+router.get("/get-one", async (req, res) => {
+    try {
+        const foundBlog = await db()
+        .collection('sample_blogs')
+        .find({})
+        .limit(1)
+        .toArray(function(err, result){
+            if (err) {
+                res.status(400).send("error fetching blogs")
+            } else {
+                res.json(result);
+            }
+        });
 
-router.get("/all", (req, res) => {
-    res.json({
-        success: true,
-        sampleBlogs
-    }).status(200);
+        if (!foundBlog) {
+            res.json({
+                success: false,
+                message: "Could not find blog in the list"
+            })
+            return
+        }
+
+        res.json({
+            success: true,
+            foundBlog
+        }).status(200);
+    }
+
+    catch (e) {
+        console.log(e);
+        res.json({
+            success: false,
+            error: String(e)
+        }).status(500);
+    }
 })
 
 
-router.get("/single/:titleToGet", ((req, res) => {
+// get one blog by title ("/single/:titleToGet")
 
-    const foundBlog = sampleBlogs.find((blog) => {
-        return blog.title === req.params.titleToGet;
-    })
+router.get("/get-one/:titleToGet", async (req, res) => {
 
-    res.json({
-        success: true,
-        foundBlog
-    }).status(200);
-}))
+    try {
+
+        const blogToFind = req.params.titleToGet;
+
+        const foundBlog = await db()
+        .collection('sample_blogs')
+        .findOne({
+            title: blogToFind
+        })
+
+        if (!foundBlog) {
+            res.json({
+                success: false,
+                message: "Could not find blog in the list"
+            })
+            return
+        }
+
+        res.json({
+            success: true,
+            foundBlog
+        }).status(200);
+    }
+
+    catch (e) {
+        console.log(e);
+        res.json({
+            success: false,
+            error: String(e)
+        }).status(500);
+    }
+})
+
+// router.get("/single/:titleToGet", ((req, res) => {
+
+//     const foundBlog = sampleBlogs.find((blog) => {
+//         return blog.title === req.params.titleToGet;
+//     })
+
+//     res.json({
+//         success: true,
+//         foundBlog
+//     }).status(200);
+// }))
 
 // PUT = UPDATE
 
-router.put("/update-one/:blogToUpdate", ((req, res) => {
+// update one blog by title ("/update-one/:blogToUpdate")
+
+router.put("/update-one/:blogToUpdate", async (req, res) => {
 
     try {
 
         const blogToFind = req.params.blogToUpdate;
 
-        const originalBlog = sampleBlogs.find((blog) => {
-            return blog.title === blogToFind;
-        })
-
-        const originalBlogIndex = sampleBlogs.findIndex((blog) => {
-            return blog.title === blogToFind;
+        const originalBlog = await db()
+        .collection('sample_blogs')
+        .findOne({
+            title: blogToFind
         })
 
         if (!originalBlog) {
@@ -175,43 +221,211 @@ router.put("/update-one/:blogToUpdate", ((req, res) => {
         updatedBlog.createdAt = new Date();
         updatedBlog.lastModified = new Date();
 
-        //pass blog data object to our validate function
-        const blogDataCheck = validateBlogData(updatedBlog);
-
-        if (blogDataCheck.isValid === false) {
-            throw Error(blogDataCheck.message)
-
-        }
-
-        sampleBlogs[originalBlogIndex] = updatedBlog
+        const updateResponse = await db()
+        .collection('sample_blogs')
+        .updateOne({
+            title: blogToFind
+        }, {
+            $set: updatedBlog
+        })
 
         res.json({
             success: true,
-        }).status(200)
-    } catch (e) {
-        // In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+            updatedBlog
+        }).status(200);
+    }
+
+    catch (e) {
         console.log(e);
         res.json({
             success: false,
             error: String(e)
         }).status(500);
     }
+})
 
-}))
+// router.put("/update-one/:blogToUpdate", ((req, res) => {
 
-router.delete("/delete/:titleToDelete", ((req, res) => {
+//     try {
 
-    const blogToDelete = req.params.titleToDelete;
-    const blogIndex = sampleBlogs.findIndex((blog) => {
-        return blog.title === blogToDelete;
-    })
+//         const blogToFind = req.params.blogToUpdate;
 
-    sampleBlogs.splice(blogIndex, 1);
+//         const originalBlog = sampleBlogs.find((blog) => {
+//             return blog.title === blogToFind;
+//         })
 
-    res.json({
-        success: true
-    }).status(200)
-}))
+//         const originalBlogIndex = sampleBlogs.findIndex((blog) => {
+//             return blog.title === blogToFind;
+//         })
+
+//         if (!originalBlog) {
+//             res.json({
+//                 success: false,
+//                 message: "Could not find blog in the list"
+//             })
+//             return
+//         }
+
+//         const updatedBlog = {}
+
+//         if (req.body.title !== undefined) {
+//             updatedBlog.title = req.body.title
+//         } else {
+//             updatedBlog.title = originalBlog.title
+//         }
+
+//         if (req.body.text !== undefined) {
+//             updatedBlog.text = req.body.text
+//         } else {
+//             updatedBlog.text = originalBlog.text
+//         }
+
+//         if (req.body.author !== undefined) {
+//             updatedBlog.author = req.body.author
+//         } else {
+//             updatedBlog.author = originalBlog.author
+//         }
+
+//         if (req.body.category !== undefined) {
+//             updatedBlog.category = req.body.category
+//         } else {
+//             updatedBlog.category = originalBlog.category
+//         }
+
+//         updatedBlog.createdAt = new Date();
+//         updatedBlog.lastModified = new Date();
+
+//         //pass blog data object to our validate function
+//         const blogDataCheck = validateBlogData(updatedBlog);
+
+//         if (blogDataCheck.isValid === false) {
+//             throw Error(blogDataCheck.message)
+
+//         }
+
+//         sampleBlogs[originalBlogIndex] = updatedBlog
+
+//         res.json({
+//             success: true,
+//         }).status(200)
+//     } catch (e) {
+//         // In the catch block, we always want to do 2 things: console.log the error and respond with an error object
+//         console.log(e);
+//         res.json({
+//             success: false,
+//             error: String(e)
+//         }).status(500);
+//     }
+
+// }))
+
+// DELETE
+
+router.delete("/delete/:titleToDelete", async (req, res) => {
+
+    try {
+
+        const blogToDelete = req.params.titleToDelete;
+
+        const deleteResponse = await db()
+        .collection('sample_blogs')
+        .deleteOne({
+            title: blogToDelete
+        })
+
+        res.json({
+            success: true
+        }).status(200)
+    }
+
+    catch (e) {
+        console.log(e);
+        res.json({
+            success: false,
+            error: String(e)
+        }).status(500);
+    }
+})
+
+// /get-multi/ (GET): get multiple sorted results
+
+router.get("/get-multi", async (req, res) => {
+
+    try {
+
+        const sort = req.query.sort;
+        const limit = parseInt(req.query.limit);
+        const skip = parseInt(req.query.skip);
+
+        const blogs = await db()
+        .collection('sample_blogs')
+        .find({})
+        .sort({
+            [sort]: -1
+        })
+        .limit(limit)
+        .skip(skip)
+        .toArray()
+
+        res.json({
+            success: true,
+            blogs
+        }).status(200);
+    }
+
+    catch (e) {
+        console.log(e);
+        res.json({
+            success: false,
+            error: String(e)
+        }).status(500);
+    }
+})
+
+// multiple delete route
+
+router.delete("/delete-multi", async (req, res) => {
+
+    try {
+
+        const blogsToDelete = req.body.blogsToDelete;
+
+        const deleteResponse = await db()
+        .collection('sample_blogs')
+        .deleteMany({
+            title: {
+                $in: blogsToDelete
+            }
+        })
+
+        res.json({
+            success: true
+        }).status(200)
+    }
+
+    catch (e) {
+        console.log(e);
+        res.json({
+            success: false,
+            error: String(e)
+        }).status(500);
+    }
+})
+
+
+// router.delete("/delete/:titleToDelete", ((req, res) => {
+
+//     const blogToDelete = req.params.titleToDelete;
+//     const blogIndex = sampleBlogs.findIndex((blog) => {
+//         return blog.title === blogToDelete;
+//     })
+
+//     sampleBlogs.splice(blogIndex, 1);
+
+//     res.json({
+//         success: true
+//     }).status(200)
+// }))
 
 
 module.exports = router;
